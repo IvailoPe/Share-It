365550fc859c8ec30e940e382a7310714902e0ad
IvailoPe <Ivailo.greta@gmail.com>
2024-04-01 23:56:24 +0300

    Adding all services

---
+++ frontend/src/app/post-info.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API } from './environment-vars';
+import { postInterface, commentInterface } from './interfaces/post-interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostInfoService {
+  constructor(private http:HttpClient) { }
+
+  createNewPost(title:string,body:string,id:string){
+    return this.http.post<postInterface>(API + "post/create",{
+      title,
+      body,
+      id
+    })
+  }
+
+  getAllUserPosts(id:string) : Observable<postInterface[]>{
+    return this.http.get<postInterface[]>(API + "post/" + id)
+  }
+
+  deleteUserPost(id:string){
+    return this.http.delete(API + "post/" + id)
+  }
+
+  getAllPosts(exceptUserId?:string){
+    if(exceptUserId){
+      const queryParams = new URLSearchParams({exceptUserId:exceptUserId})      
+      return this.http.get<postInterface[]>(API + "post?" + queryParams.toString())
+    }
+    return this.http.get<postInterface[]>(API + "post")
+  }
+
+  likePost(postId:string,userId:string){
+    return this.http.post(API + "post/like/" + postId,{
+       userId
+    })
+  }
+
+  postPostComment(postId:string,userId:string,comment:string){
+    return this.http.post<commentInterface>(API + "post/comment/" + postId,{
+      userId,
+      comment
+   })
+  }
+
+  getAllPostComments(postId:string){
+    return this.http.get<commentInterface[]>(API + "post/comment/" + postId)
+  }
+
+  likePostComment(postId:string,commentId:string,userId:string){
+    return this.http.post(API + "post/comment/like/" + postId,{
+      userId,
+      commentId
+    })
+  }
+
+  getSinglePost(postId:string){
+    return this.http.get<postInterface>(API + "post/singlepost/" + postId)
+  }
+
+}
